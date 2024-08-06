83c75090c2016682dfdf9765044d165c63cb841a
jagadeeswaranp <jagadeeswaranp@gislen.com>
2024-08-06 15:01:49 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Spline from '@splinetool/react-spline';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='spline-container'>
+    <div className='nav'>
+      <ul>
+        <li>Lova</li>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        </ul>
+        </div> 
+    <div className='spline'> <Spline
+        scene="https://prod.spline.design/C8ivOptgnpunJOB0/scene.splinecode" 
+      /></div>
+     <div className='footer'>
+      <p>
+      Quench your thirst, the extraordinary way! Water for life, purity guaranteed. Refreshing drops for a rejuvenating experience. Drink the difference, sip by sip.
+      </p>
+      <button>Explore Products</button>
+     </div>
+
+  </div>
   );
 }
 
